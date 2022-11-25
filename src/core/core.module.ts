f925d0854cc5b941361503ef413a0ad84229a752
OlgaSavitsk <olbonya.89@mail.ru>
2022-11-26 00:29:50 +0300

    feat: refactor for deploy

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { AccessAuthGuard } from './guards/access.guard';
 
 @Module({
   controllers: [],
-  providers: [
-    /* {
-      provide: APP_GUARD,
-      useClass: AccessAuthGuard,
-    }, */
-  ],
+  providers: [],
   imports: [],
 })
 export class CoreModule {}
